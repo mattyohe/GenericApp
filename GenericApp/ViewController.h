7b7d99ae15d6db4d3c39bfe93e76eb396e417682
Matt Yohe <matt.yohe@gmail.com>
2012-09-13 17:43:55 -0400

    Initial Commit

---
+++ GenericApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GenericApp
+//
+//  Created by Matt Yohe on 9/13/12.
+//  Copyright (c) 2012 Matt Yohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
